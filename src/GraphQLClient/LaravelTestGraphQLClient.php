6c858868d7e706bcbb888964ef030e4252b5c4d1
Christian Goltz <goltzchristian@gmail.com>
2018-01-20 20:27:50 +0100

    Laravel client fixes

--- src/GraphQLClient/LaravelTestGraphQLClient.php
+++ src/GraphQLClient/LaravelTestGraphQLClient.php
@@ -34,7 +34,7 @@ class LaravelTestGraphQLClient extends Client
     protected function postQuery(array $data): array
     {
         $response = $this->post($this->getBaseUrl(), $data);
-        $responseBody = json_decode($response->getContent(), true);
+        $responseBody = json_decode($this->response->getContent(), true);
 
         if (isset($responseBody['errors'])) {
             throw new GraphQLException(sprintf('Mutation failed with error %s', json_encode($response['errors'])));
